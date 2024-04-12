c98bba0a2af394212f4e28586783880f045ccd06
Gambit <efin2012@yandex.ru>
2024-04-13 01:38:58 +0300

    TASK-1 - Добавлены новые парсеры

---
+++ ObjectOriented/Classes/Abstraction/Parsing/SportSiteParser.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace ObjectOriented\Classes\Abstraction\Parsing;
+
+// конкретная реализация парсера
+class SportSiteParser extends Parser
+{
+    public function parse(): string
+    {
+        return "SportSiteParser: <html><head><title></title></head><body>...Парсинг спортивных сайтов...";
+    }
+}
